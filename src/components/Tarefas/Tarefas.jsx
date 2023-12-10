b5a829cee69e4abdefd492196e2ddeb2a387d1dd
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2023-12-10 12:01:26 -0300

    update

--- src/components/Tarefas/Tarefas.jsx
+++ src/components/Tarefas/Tarefas.jsx
@@ -4,16 +4,16 @@ import { StyledTarefas } from "./styled";
 import { CiTrash } from "react-icons/ci";
 import { CiCircleCheck } from "react-icons/ci";
 
-export default function Tarefas() {
+export default function Tarefas(prop) {
     return (
         <StyledTarefas>
 
             <div id="tarefa">
                 <CiCircleCheck />
                 <div id="nomeTarefa">
-                    <h2>nome da tarefa</h2>
+                    <h2>{prop.tarefa}</h2>
                 </div>
-                <p>responsavel </p>
+                <p>{prop.nome}</p>
                 <CiTrash />
 
             </div>
--- src/pages/Inicial/Inicial.jsx
+++ src/pages/Inicial/Inicial.jsx
@@ -3,19 +3,35 @@ import { StyledInicial } from "./styled";
 import { CiCirclePlus } from "react-icons/ci";
 import { RiSearchEyeLine } from "react-icons/ri";
 import Tarefas from "../../components/Tarefas/Tarefas";
+// import { useState } from "react";
+
+
 
 export default function Inicial() {
+
+    const listaMinhasTarefas = [
+        {id:1,tarefa:'fazer café', nome:'marcos'},
+        {id:2,tarefa:'beber agua', nome:'junior'}
+    ]
+
+    // const [tarefa, setTarefa] = useState(listaMinhasTarefas)
+
+    function addTarefas(){
+        const novaTarefa = prompt()
+
+        // setTarefa(novaTarefa)
+        listaMinhasTarefas.push(novaTarefa)
+        
+        console.log(listaMinhasTarefas)
+    }
+/* */
     return (
         <StyledInicial>
+            {console.log(listaMinhasTarefas)}
             <div id="container">
 
                 <div id="tarefas">
-                    <Tarefas />
-                    <Tarefas />
-                    <Tarefas />
-                    <Tarefas />
-                    <Tarefas />
-                    <Tarefas />
+                    {listaMinhasTarefas.map((t) => <Tarefas tarefa={t.tarefa} nome={t.nome} key={t.id} />)}
                 </div>
 
                 <div id="cx">
@@ -23,7 +39,7 @@ export default function Inicial() {
                         <textarea id="texto" className="imputMesnsagem" name="msg" rows="2" cols="50"></textarea>
 
                         <div id="botoes">
-                            <button>  <CiCirclePlus />  NOVA TAREFA</button>
+                            <button onClick={addTarefas} >  <CiCirclePlus />  NOVA TAREFA</button>
                             <button> <RiSearchEyeLine /> VER TODAS AS TAREFAS</button>
 
                         </div>

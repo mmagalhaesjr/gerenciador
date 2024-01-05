098b5d7b686b24459aeca10ac0139e9c0c3134f7
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-01-05 15:39:30 -0300

    update

--- src/components/Tarefas/styled.js
+++ src/components/Tarefas/styled.js
@@ -14,7 +14,7 @@ justify-content: center;
     margin-bottom: 3%;
     border-radius: 50px;
    
-    background: ${(props) => (props.concluida ? '#08f41ca5' : 'radial-gradient(circle, rgba(162,171,180,0.29315476190476186) 0%, rgba(200,204,224,0.27074579831932777) 100%)')};
+    background: ${(prop) => (prop.concluida ? '#ffe100d9' : 'radial-gradient(circle, rgba(162,171,180,0.29315476190476186) 0%, rgba(200,204,224,0.27074579831932777) 100%)')};
 
     display: flex;
    
@@ -39,6 +39,9 @@ font-size: 1rem;
 }
 #tarefa p{
 font-size:.7rem;
+}
+#check,#lixo{
+    font-size: 1.5rem;
 }
 #check:hover{
     color: rgb(238,97,16);
